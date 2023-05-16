02dfd3c5153da81b3c2e12ac22ff6e2e3209468c
panditprogrammer <panditprogrammer@gmail.com>
2023-05-16 11:18:29 +0530

    connection established

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Call Me | Pandit Programmer</title>
+</head>
+
+<body>
+    <h1>Hello world</h1>
+
+
+    <script>
+        var conn = new WebSocket('ws://localhost:8080');
+        conn.onopen = function(e) {
+            console.log("Connection established!");
+        };
+
+        conn.onmessage = function(e) {
+            console.log(e.data);
+        };
+    </script>
+</body>
+
+</html>
